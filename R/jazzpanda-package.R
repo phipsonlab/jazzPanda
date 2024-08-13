7ad512f481388afcf8c557d670d88847226e2008
Dharmesh Bhuva <dharmesh.bhuva@adelaide.edu.au>
2024-08-13 10:58:34 +0930

    bugfix: URLs in documentation

--- R/jazzpanda-package.R
+++ R/jazzpanda-package.R
@@ -10,9 +10,7 @@
 #' }
 #' @usage data(rep1_sub)
 #' @return List
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep1/Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
 "rep1_sub"
 
 
@@ -32,9 +30,7 @@
 #' }
 #' @usage data(rep1_clusters)
 #' @return  data frame
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep1/Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
 "rep1_clusters"
 
 #' Rep1 negative control genes within the selected region.
@@ -51,9 +47,7 @@
 #' }
 #' @usage data(rep1_neg)
 #' @return List
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep1/Xenium_FFPE_Human_Breast_Cancer_Rep1_outs.zip}
 "rep1_neg"
 
 
@@ -69,9 +63,7 @@
 #' }
 #' @usage data(rep2_sub)
 #' @return List
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep2/Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
 "rep2_sub"
 
 
@@ -91,9 +83,7 @@
 #' }
 #' @usage data(rep2_clusters)
 #' @return  data frame
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep2/Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
 "rep2_clusters"
 
 #' Rep2 negative control genes within the selected region.
@@ -110,7 +100,5 @@
 #' }
 #' @usage data(rep2_neg)
 #' @return List
-#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2/
-#' Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
+#' @source \url{https://cf.10xgenomics.com/samples/xenium/1.0.1/Xenium_FFPE_Human_Breast_Cancer_Rep2/Xenium_FFPE_Human_Breast_Cancer_Rep2_outs.zip}
 "rep2_neg"
